2b1e328f353ea7c7686f552589f3c1bb27589ed5
Eduardo <eduardocasagrande71@gmail.com>
2023-03-18 20:14:10 -0300

    Configurações globais

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Dashboard from "./Pages/Dashboard";
+import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<Dashboard />
+		<App />
 	</React.StrictMode>,
 );
